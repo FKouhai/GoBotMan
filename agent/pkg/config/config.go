8634da45b45b2b1f48aaee1a8a36c1e814ff303c
zakk <81378657+badgersec@users.noreply.github.com>
2022-04-09 08:20:43 -0500

    Update config.go

--- agent/pkg/config/config.go
+++ agent/pkg/config/config.go
@@ -6,32 +6,23 @@ import (
 	"io/ioutil"
 )
 
-var (
-	TcpAddr string
-	Shell  string
-	Platform string
-	config *confStruct
-)
-
-type confStruct struct {
-	TcpAddr string `json:"TCPAddr"`
-	Shell string `json : "Shell"`
-	Plaftorm string `json : "Platform"`
+type Config struct {
+	TcpAddr  string `json:"TCPAddr"`
+	Shell    string `json:"Shell"`
+	Plaftorm string `json:"Platform"`
 }
 
-func ReadConfig() error {
+func NewConfig() (*Config, error) {
+	var config *Config
 	file, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		fmt.Printf("Error reading config")
-		return err
+		fmt.Println("Error reading config", err)
+		return nil, err
 	}
 	err = json.Unmarshal(file, &config)
 	if err != nil {
-		fmt.Println(err.Error())
-		return err
+		fmt.Println("error unmarshalling config", err)
+		return nil, err
 	}
-	TcpAddr = config.TcpAddr
-	Shell = config.Shell
-	Platform = config.Plaftorm
-	return nil
+	return config, err
 }
