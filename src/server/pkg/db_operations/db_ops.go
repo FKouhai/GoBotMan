bc1f839eb7e49391b012469db928a15465615dfd
FKouhai <frandres00@gmail.com>
2022-04-13 19:30:31 +0200

    Included creation of db in db_ops

--- src/server/pkg/db_operations/db_ops.go
+++ src/server/pkg/db_operations/db_ops.go
@@ -7,7 +7,26 @@ import (
   "os"
 )
 
-type UserModel struct {
+type StationModel struct {
   ID int `db:"id"`
   Station string `db:"station"`
+}
+
+func (u *StationModel) TableName() string {
+  return "stations"
+}
+
+func createDB(name string, user string, password string, dbHost string) *sql.DB{
+  connString := user + ":" + password + "tcp(" + dbHost + ")/"
+  db, err := sql.Open("mysql", connString)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+  _, err := db.Exec("CREATE DATABASE IF NOT EXISTS"+name)
+  if err != nil {
+    panic(err)
+  }
+  return db
+  db.Close()
 }
